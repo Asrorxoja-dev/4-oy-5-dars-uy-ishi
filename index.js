48d328befe8e97ac2d47fddb86f06e85553afbd6
asrorxoja07 <asrorxojaisomiddinov21@gmail.com>
2023-12-16 20:54:23 +0500

    while-masalalari

---
+++ index.js
@@ -0,0 +1,88 @@
+
+// 1-masala
+
+// let a = 1;
+// let b = 5;
+//  let i = 1;
+// while ( i <= b) {
+//     a *=i;
+
+//     i++;
+// }
+
+// console.log(a);
+
+
+
+
+// 2-masala
+
+
+// let son = 1234;
+// let sum = 0;
+
+
+// while (son > 0) {
+ 
+//   son = Math.floor(son / 10);
+
+ 
+//   sum++;
+// }
+
+
+// console.log(sum);
+
+
+
+// 3-masala
+
+// let son =8;
+// let i=1;
+// sum = 1;
+// while( i <= son) {
+//     sum *=i;
+
+//     i++;
+// }
+// console.log(sum);
+
+
+
+// 5-masala
+
+// let n = 20;
+// let i=1;
+// let sum=1;
+// while( i <= n ) {
+//     if ( n % i == 1) {
+//         sum=sum+=i;
+     
+ 
+//     }
+
+//     if (sum==2){
+//         sum=sum+=i;
+        
+//     }
+
+//     i++;
+// }
+
+// console.log(sum);
+
+
+
+// 6-masala
+
+
+
+// let son =123;
+
+// let birliklar = Math.floor(son % 10);
+// let onliklar = Math.floor(son / 10) %10;
+// let yuzliklar =Math.floor( son / 100);
+
+
+// console.log( birliklar,onliklar,yuzliklar );
+
